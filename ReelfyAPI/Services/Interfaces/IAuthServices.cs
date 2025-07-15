2d1be904cdea66d57aaaabeaa2c161bd237a5014
Thiago Peixoto <thiago.peixots@gmail.com>
2025-07-15 15:04:59 -0300

    Fix: Arrumei os bugs de referência dos contratos

--- ReelfyAPI/Services/Interfaces/IAuthServices.cs
+++ ReelfyAPI/Services/Interfaces/IAuthServices.cs
@@ -1,17 +1,28 @@
-﻿using ReelfyAPI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using ReelfyAPI.Models;
+using ReelfyAPI.Models.DTO;
 
 namespace ReelfyAPI.Services.Interfaces
 {
     public interface IAuthServices
     {
-        public Task<User> Register(User user, string password);
-        public Task<User> Login(string email, string password);
+        public Task<UserResponseDTO?> Register(UserRegisterDTO userDto);
+
+        public Task<UserResponseDTO?> Login(UserLoginDTO loginDto);
+
         public Task<bool> UserExists(string email);
-        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
-        public bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt);
-        public string CreateToken(User user);
-        public Task<User> GetUserById(int id);
-        public Task<User> GetUserByEmail(string email);
-        public Task<IEnumerable<User>> GetAllUsers();
+
+        public Task<UserResponseDTO?> GetUserById(int id);
+
+        public Task<UserResponseDTO?> GetUserByEmail(string email);
+
+        public Task<IEnumerable<UserResponseDTO>> GetAllUsers();
+
+        public Task<UserResponseDTO?> UpdatePassword(UpdatePasswordDTO update, string newPassword);
+
+        public Task<bool> DeleteUser(int id);
+        public string CreateToken(UserResponseDTO user);
+
+
     }
 }
