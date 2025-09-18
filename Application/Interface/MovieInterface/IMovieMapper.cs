8c814fd6e8f452f2ea29f84d498c1ecbad910c13
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-18 14:42:55 -0300

    Interface do Movi *alterar para content

---
+++ Application/Interface/MovieInterface/IMovieMapper.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTO.Content;
+using Domain.Models.Contents;
+
+
+namespace Domain.Interface.Services.Movie;
+
+public interface IMovieMapper
+{
+    //Entity
+    public Content ToEntity(FavoriteContentDTO favoriteMovieDTO);
+    public IEnumerable<Content> ToEntities(IEnumerable<FavoriteContentDTO> favoriteMovies);
+
+}
---
+++ Application/Interface/MovieInterface/IMovieService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTO.Content;
+
+namespace Application.Interface.MovieInterface
+{
+    public interface IMovieService
+    {
+        Task<FavoriteContentDTO> Favorite(FavoriteContentDTO movie);
+        Task<bool> RemoveFavorite(int id);
+
+    }
+}
