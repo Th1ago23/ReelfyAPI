9f681a084392bf1c34e7370232a54f32cc6b999c
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-18 14:45:42 -0300

    Melhoria de arquitetura

---
+++ Domain/Models/Users/User.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Models.Contents;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models.Users;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public int Age { get; set; }
+
+    [Required]
+    [MaxLength(90)]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public byte[] PasswordHash { get; set; }
+
+    [Required]
+    public byte[] PasswordSalt { get; set; }
+
+    [Required]
+    public string PhoneNumber { get; set; }
+
+    public ICollection<Content> Movies { get; set; } = new List<Content>();
+    public int PreferenceId { get; set; }
+    public Preference Preference { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+
+}
