53abd5964e5afe6988c4cc599c47e59c78b5668e
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-18 14:43:05 -0300

    Interface dos usuarios

---
+++ Application/Interface/UserInterface/IAuthServices.cs
@@ -0,0 +1,27 @@
+﻿using Application.DTO.Content;
+using Application.DTO.Returns;
+using Application.DTO.Users;
+
+namespace Application.Interface.UserInterface;
+
+public interface IAuthServices
+{
+    public Task<ResponseRequestDTO?> Register(UserRegisterDTO userDto);
+
+    public Task<ResponseRequestDTO?> Login(UserLoginDTO loginDto);
+
+    public Task<UserResponseDTO?> GetUserById(int id);
+
+    public Task<UserResponseDTO?> GetUserByEmail(string email);
+
+    public Task<IEnumerable<UserResponseDTO>> GetAllUsers();
+
+    public Task<UserResponseDTO?> UpdatePassword(UpdatePasswordDTO update, string newPassword);
+    public Task<FavoriteDTO> GetFavorite(int id);
+    public Task<FavoriteDTO> GetFavoriteInContext();
+    public Task<bool> VerifyUser(string email);
+
+    public Task<bool> DeleteUser(int id);
+
+
+}
