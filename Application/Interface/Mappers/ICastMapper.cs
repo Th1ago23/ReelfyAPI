76f0f29b09950fd96ac5609511f9d28e6fa1774c
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-22 14:49:27 -0300

    new mappers

--- Application/Interface/Mappers/ICastMapper.cs
+++ Application/Interface/Mappers/ICastMapper.cs
@@ -1,4 +1,4 @@
-﻿using Application.DTO.Content;
+﻿using Application.DTO.Content.Preferences;
 using Domain.Models.Contents;
 
 namespace Domain.Interface.Mappers;
@@ -6,5 +6,6 @@ namespace Domain.Interface.Mappers;
 public interface ICastMapper
 {
     public Cast ToEntity(CastAddDTO dto);
+    public CastAddDTO ToDTO(Cast cast);
     public IEnumerable<Cast> ToEntities(IEnumerable<CastAddDTO> dtos);
 }
