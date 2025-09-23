75feaabc3adf1886f8fd3f8561d7ab500d232291
Thiago Peixoto dos Santos <thiago.santos.ts3@csn.com.br>
2025-09-23 14:53:52 -0300

    Fix: Alterei os atributos como nullables.

--- Application/DTO/Content/FavoriteCountDTO.cs
+++ Application/DTO/Content/FavoriteCountDTO.cs
@@ -2,7 +2,7 @@
 
 namespace Application.DTO.Content
 {
-    public record FavoriteCountDTO(string contentName, Category category, int contentId, int usersCount)
+    public record FavoriteCountDTO(string? contentName, Category? category, int contentId, int usersCount)
     {
     }
 }
